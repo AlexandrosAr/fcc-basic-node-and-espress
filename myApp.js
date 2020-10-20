2ae35f7faa11e4a0ac57a398b455b59d1ced9129
aarapidis <aarapidis@deloitte.gr>
2020-10-20 10:13:04 +0300

    Use sendFile to send a html in the browser

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ console.log('Hello World');
 
 /** 2) A first working Express Server */
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
